737c15b4a975c41fa52c1706deb94f1bba79a5d2
tezzzka <47881130+tezzzka@users.noreply.github.com>
2020-10-14 18:01:10 +0500

    Update main.go

--- main.go
+++ main.go
@@ -3,20 +3,55 @@ package main
 import (
 	"fmt"
 
-	hm "github.com/tezzzka/go-intro/homework2"
+	"github.com/tezzzka/go-intro/homework4"
+	"github.com/tezzzka/go-intro/homework4/abstract"
 )
 
+type userService struct {
+	storage homework4.AccountStorage
+}
+
+func (s *userService) GetByID(id int64) (*abstract.User, error) {
+	return s.storage.GetByID(id)
+}
+
+func (s *userService) NewUser(ID int64, Login string) error {
+	return s.storage.NewUser(&abstract.User{
+		ID:    ID,
+		Login: Login,
+	})
+}
+
+func (s *userService) SetPasswd(id int64, passwd string) error {
+	s.storage.SetPasswd(id, passwd)
+	return nil
+}
+
+func (s *userService) ResetPasswd(id int64, newpasswd string, root string) error {
+	s.storage.ResetPasswd(id, newpasswd, root)
+	return nil
+}
+
 func main() {
+	mapStorage := abstract.Init()
 
-	X, e := hm.EvenOdd(4)
-	Y := hm.Del(6)
-	if e != nil {
-		panic(e)
+	service := &userService{
+		storage: mapStorage,
 	}
-	fmt.Println("Чет/нечет true/false: ", X)
-	fmt.Println("Деление на 3 true/false: ", Y)
 
-	//hm.Fib(100)
-	hm.FibonacciWrapper(20)
+	service.NewUser(1, "tzk")
+
+	if user, e := service.GetByID(1); e != nil {
+		panic("Cannot Get User By ID. Sorry.")
+	} else {
+		fmt.Println(user)
+	}
+
+	if pw := service.SetPasswd(1, "hello"); pw != nil {
+		panic("Cannot set any password. Sorry.")
+	}
+
+	service.ResetPasswd(1, "PA$$W0RD", "Hello")
+	fmt.Println(service.GetByID(1))
 
 }
